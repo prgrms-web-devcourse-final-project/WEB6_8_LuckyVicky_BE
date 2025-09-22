6fb77721c52ab6b28dc5797bda5e1fe8f4100d5a
임홍담 <107115139+ahdeka@users.noreply.github.com>
2025-09-22 15:43:40 +0900

    [feat] User 엔티티 생성 (#22)

---
+++ src/main/java/com/back/domain/user/entity/Provider.java
@@ -0,0 +1,8 @@
+package com.back.domain.user.entity;
+
+public enum Provider {
+    LOCAL,      // 일반 회원가입
+    GOOGLE,     // 구글
+    KAKAO,      // 카카오
+    NAVER       // 네이버
+}
---
+++ src/main/java/com/back/domain/user/entity/Role.java
@@ -0,0 +1,7 @@
+package com.back.domain.user.entity;
+
+public enum Role {
+    USER,       // 일반 사용자
+    ADMIN,      // 관리자
+    ROOT        // 최고 관리자
+}
