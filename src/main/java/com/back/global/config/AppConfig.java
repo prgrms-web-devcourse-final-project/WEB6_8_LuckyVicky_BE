6d05bedf9913cda3bacf9ba47fffaa19a030f503
yeeJin227 <dpwls99600@gmail.com>
2025-10-02 01:54:42 +0900

    infra/Ap pConfig 생성

---
+++ src/main/java/com/back/global/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.back.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+    private static String cookieDomain;
+    private static String siteFrontUrl;
+    private static String siteBackUrl;
+
+    public AppConfig(
+            @Value("${custom.site.cookieDomain}") String cookieDomain,
+            @Value("${custom.site.frontUrl}") String siteFrontUrl,
+            @Value("${custom.site.backUrl}") String siteBackUrl
+    ) {
+        AppConfig.cookieDomain = cookieDomain;
+        AppConfig.siteFrontUrl = siteFrontUrl;
+        AppConfig.siteBackUrl = siteBackUrl;
+    }
+
+    public static String getCookieDomain() {
+        return cookieDomain;
+    }
+
+    public static String getSiteFrontUrl() {
+        return siteFrontUrl;
+    }
+
+    public static String getSiteBackUrl() {
+        return siteBackUrl;
+    }
+}
