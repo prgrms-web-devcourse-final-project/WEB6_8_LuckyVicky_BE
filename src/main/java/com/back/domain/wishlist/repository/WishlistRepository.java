bd948e0eb9533862e7873f506caf1735692dd191
yoostill <yhs000323@gmail.com>
2025-10-14 17:51:14 +0900

    refactor/354 레파지토리에 찜 기능 조회 추가

--- src/main/java/com/back/domain/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/back/domain/wishlist/repository/WishlistRepository.java
@@ -1,14 +1,32 @@
 package com.back.domain.wishlist.repository;
 
 import com.back.domain.wishlist.entity.Wishlist;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     // 찜 등록 여부 조회
     boolean existsByUserIdAndProductId(Long userId, Long productId);
+
     // 찜 삭제
     void deleteByUserIdAndProductId(Long userId, Long productId);
+
     // 상품별 찜 개수 조회
     Long countByProductId(Long productId);
+
+    /**
+     * 대시보드용: 사용자의 찜한 상품 목록 조회 (페이징, Product와 User fetch join)
+     * Product의 images는 별도로 BatchSize로 처리됨
+     */
+    @Query("SELECT w FROM Wishlist w " +
+            "JOIN FETCH w.product p " +
+            "JOIN FETCH p.user " +
+            "WHERE w.user.id = :userId " +
+            "AND p.isDeleted = false " +
+            "ORDER BY w.createDate DESC")
+    Page<Wishlist> findWishlistsByUserIdForDashboard(@Param("userId") Long userId, Pageable pageable);
 
 }
